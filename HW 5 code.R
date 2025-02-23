09c0d3a509fa85ab0d493c08f8e7a894c5ae18f3
anyasuko <166669795+anyasuko@users.noreply.github.com>
2025-02-23 15:11:36 -0500

    Update HW 5 code.R

--- HW 5 code.R
+++ HW 5 code.R
@@ -7,8 +7,8 @@ library(jsonlite)
 
 ######step 1
 #step 1 part 1
-current.filename <- "The Front Bottoms-Talon Of The Hawk-Au Revoir (Adios).json"|>
-  split <- str_split(., "-")
+current.filename <- "The Front Bottoms-Talon Of The Hawk-Au Revoir (Adios).json"
+split <- current.filename %>% str_split("-")
 artist <- split[[1]][1]
 track <-split[[1]][2]
 filename <-str_sub(split[[1]][3],,-6)
@@ -35,7 +35,7 @@ clean <- function(data){
 
 ###### Step 2
 list.of.filenames <- list.files("EssentiaOutput")%>%
-  -which(endsWith('.csv'))
+  .[!endsWith(., ".csv")]
 
 frame <- data.frame(overall.loudness = rep(x=NA, times=length(list.of.filenames)),
                     spectral.energy = rep(x=NA, times=length(list.of.filenames)),
@@ -53,39 +53,40 @@ for (i in 1:length(list.of.filenames)){
   split = str_split(list.of.filenames[i], "-", simplify=TRUE)
   to.sort <- fromJSON(paste0("EssentiaOutput/",list.of.filenames[i])) %>%
     clean() %>%
-    mutuate(
+    mutate(
       curr.artist = split[1],
       curr.album = split[2],
       curr.track = str_sub(split[3], end=-6) 
-    )|>
-  frame[i,] <- .
+    )
+  frame[i,] <- to.sort
 }
 
 ######step 3
 #step 3 part 1
 info <- read.csv("EssentiaOutput/EssentiaModelOutput.csv")%>%
-  with(
-    valence <- rowMeans(info[,c("deam_valence", "emo_valence", "muse_valence")], na.rm=TRUE),
-    arousal <- rowMeans(info[,c("deam_arousal", "emo_arousal", "muse_arousal")], na.rm=TRUE),
-    aggressive <-rowMeans(info[,c("eff_aggressive","nn_aggressive")], na.rm=TRUE),
-    happy <-rowMeans(info[,c("eff_happy","nn_happy")], na.rm=TRUE),
-    party <-rowMeans(info[,c("eff_party","nn_party")], na.rm=TRUE),
-    relaxed <-rowMeans(info[,c("eff_relax","nn_relax")], na.rm=TRUE),
-    sad <-rowMeans(info[,c("eff_sad","nn_sad")], na.rm=TRUE),
-    acoustic <-rowMeans(info[,c("eff_acoustic","nn_acoustic")], na.rm=TRUE),
-    electric <-rowMeans(info[,c("eff_electronic","nn_electronic")], na.rm=TRUE),
-    instrumental <-rowMeans(info[,c("eff_instrumental","nn_instrumental")], na.rm=TRUE)
+  mutate(
+    valence <- rowMeans(.[,c("deam_valence", "emo_valence", "muse_valence")], na.rm=TRUE),
+    arousal <- rowMeans(.[,c("deam_arousal", "emo_arousal", "muse_arousal")], na.rm=TRUE),
+    aggressive <-rowMeans(.[,c("eff_aggressive","nn_aggressive")], na.rm=TRUE),
+    happy <-rowMeans(.[,c("eff_happy","nn_happy")], na.rm=TRUE),
+    party <-rowMeans(.[,c("eff_party","nn_party")], na.rm=TRUE),
+    relaxed <-rowMeans(.[,c("eff_relax","nn_relax")], na.rm=TRUE),
+    sad <-rowMeans(.[,c("eff_sad","nn_sad")], na.rm=TRUE),
+    acoustic <-rowMeans(.[,c("eff_acoustic","nn_acoustic")], na.rm=TRUE),
+    electric <-rowMeans(.[,c("eff_electronic","nn_electronic")], na.rm=TRUE),
+    instrumental <-rowMeans(.[,c("eff_instrumental","nn_instrumental")], na.rm=TRUE)
   )%>%
-  colnames()=="eff_timbre_bright" <-"timbreBright"%>%
-  info[, c("artist", "album", "track", "valence", "arousal", "aggressive", "happy", "party", "relaxed", "sad", "acoustic", "electric", "instrumental", "timbreBright")]
-
+  rename(timbreBright = eff_timbre_bright) %>%
+  select(artist,album,track,valence,arousal,aggressive,happy,party,relaxed,sad,acoustic,electric,intstrumental,timbreBright)
 
 ######step 4
 #step 4 part 1
-lyrics <- read_csv("LIWCOutput/LIWCOutput.csv") |>
+lyrics <- read_csv("LIWCOutput/LIWCOutput.csv")
 
-#step 4 part 2
-merged.two <- merge(. , info, by=c("artist", "album", "track"))|>
-merged <- merge(., frame, by=c("artist", "album", "track"))%>%
-  colnames() == "function" <- "funct"
-
+merged<- lyrics|>
+  merge(info, by = c("artist", "album", "track"))|>
+  merge(frame, by = c("artist", "album", "track"))|>
+  {
+    colnames(.)[colnames(.)=="function"]<- "funct"
+    .
+  }
